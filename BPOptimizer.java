bd5e084241bd6df39a2d16132f5b57e42519d484
Anthony Dubis <ajd2194@columbia.edu>
2015-04-17 14:37:26 -0400

    Initial commit with skelton code to read the config and query file.

---
+++ BPOptimizer.java
@@ -0,0 +1,40 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class BPOptimizer {
+    private int r;
+    private int t;
+    private int l;
+    private int m;
+    private int a;
+    private int f;
+    
+    /*
+     * Read the cost values from the config.txt file
+     */
+    private void getCostValues(String filename) throws IOException {
+        Properties prop = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + filename + "' not found in the classpath");
+        }
+        
+        r = Integer.parseInt(prop.getProperty("r"));
+        t = Integer.parseInt(prop.getProperty("t"));
+        l = Integer.parseInt(prop.getProperty("l"));
+        m = Integer.parseInt(prop.getProperty("m"));
+        a = Integer.parseInt(prop.getProperty("a"));
+        f = Integer.parseInt(prop.getProperty("f"));
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BPOptimizer optimizer = new BPOptimizer();
+        optimizer.getCostValues(args[1]);
+        System.out.println("Cost values are r=" + optimizer.r + ", t=" + optimizer.t + ", l=" + optimizer.l
+                          + ", m=" + optimizer.m + ", a=" + optimizer.a + ", f=" + optimizer.f);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,10 @@
+
+***** Sources *****
+1) To learn how to read variables from .properties files, the following link
+was referenced:
+http://crunchify.com/java-properties-file-how-to-read-config-properties-values-in-java/
+
+2) To learn how to create Java Makefiles, the following link was reference: 
+http://www.cs.swarthmore.edu/~newhall/unixhelp/javamakefiles.html
+
+
