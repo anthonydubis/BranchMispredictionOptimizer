30e7f34f340dd17ec079baa4a28702e95a1eed47
Anthony Dubis <ajd2194@columbia.edu>
2015-04-22 17:29:53 -0400

    Additional testing using branch_mispred.c

--- template/branch_mispred.c
+++ template/branch_mispred.c
@@ -145,10 +145,8 @@ int main(int argc, char * argv[]) {
   // you need to substitute this part for your own branch which you want to measure.
 
   for(i = 0; i < LOOP; i++) {
-  if (t4[o4[i]] && t3[o3[i]]) {
-          answer[j] = i;
-              j+= (t1[o1[i]] & t2[o2[i]]);
-  }
+            answer[j] = i;
+                j+= (t1[o1[i]] & t2[o2[i]] & t3[o3[i]] & t4[o4[i]]);
   }
 
   // Read the counter
